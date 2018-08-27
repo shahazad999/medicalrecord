3c7cf00f9f7e0a08db97cf7d5578812fbf2317da
Shahazad <p.shahazad1@gmail.com>
2018-08-27 18:08:03 +0530

    unit test commit

--- unittest.go
+++ unittest.go
@@ -1,15 +1,34 @@
-package shim
+package main
+
 import (
-	"encoding/json"
-	"fmt"
 	"testing"
-	"github.com/hyperledger/fabric/core/chaincode/shim" 
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-func TestaddMedicalRecord (t *testing.T) {
+type CustomMockStub struct {
+	stub           *MockStub
+	CertAttributes map[string][]byte
+}
+
+var newmr = `{"id":"110","name":"rajest","weight":"70","age":"21"}`
+
+//test weather addmedicalrecord is invoked sussesfully
+func TestaddMedicalRecord(t *testing.T) {
 	attributes := make(map[string][]byte)
-	stub :=shim.NewCustomMockStub("mockStub", new(SampleChaincode), attributes)
-	if stub == nil{
+	attributes["id"] = []byte("218")
+	attributes["name"] = []byte("raksho")
+	attributes["weight"] = []byte("76")
+	attributes["age"] = []byte("45")
+
+	stub := shim.NewCustomMockStub("mockStub", new(SampleChaincode), attributes)
+	if stub == nil {
 		t.Fatelf("MockStub creation failed")
 	}
+	_, err := stub.MockInvoke("addMedicalRecord", []string{})
+	if err != nil {
+		t.Fatalf("Expected addMedicalRecord to be invoked")
+	}
 }
+
+//
